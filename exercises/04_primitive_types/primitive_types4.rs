8cd7be98a8f2483484f7b139347a26ecda0fe699
steve wagner <ciroque@live.com>
2025-09-17 11:53:26 -0700

    primitive types complete

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -6,11 +6,12 @@ fn main() {
 mod tests {
     #[test]
     fn slice_out_of_array() {
+        let expected = [2, 3, 4];
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let actual = &a[1..4];
 
-        assert_eq!([2, 3, 4], nice_slice);
+        assert_eq!(expected, actual);
     }
 }
